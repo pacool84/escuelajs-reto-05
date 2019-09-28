ae020c2c91f2624f8bd03a81c04ea496a4c6056a
Francisco Lopez Campos <mf0lope@mx.wal-mart.com>
2019-09-28 12:18:36 -0500

    Solucion del reto 5 Francisco Lopez

--- src/index.js
+++ src/index.js
@@ -1,38 +1,73 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+let API = 'https://rickandmortyapi.com/api/character/';
+//let API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+
+function kepplocalstorage(data) {
+  console.log(data);
+  localStorage.removeItem('next_fetch');
+  localStorage.setItem('next_fetch', data);
+}
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      if (localStorage.getItem('next_fetch')) {
+        API = localStorage.getItem('next_fetch');
+      }
+      API = response.info.next;
+      kepplocalstorage(API);
+
+      if (API === '') {
+        throw new Error('alalsl');
+      }
+
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
+      let output = characters
+
+        .map(character => {
+          return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
+    `;
+        })
+        .join('');
       let newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
-}
+    .catch(error => showErrorMessage());
+};
 
-const loadData = () => {
-  getData(API);
-}
+const showErrorMessage = () => {
+  let newItem = document.createElement('section');
+  newItem.classList.add('error');
+  newItem.innerHTML = 'Ya no hay personajes...';
+  $app.appendChild(newItem);
+  intersectionObserver.disconnect();
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+const loadData = async () => {
+  try {
+    await getData(API);
+  } catch (error) {
+    console.error('Algo Ocurrio');
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+};
+
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: '0px 0px 100% 0px'
+  }
+);
 
 intersectionObserver.observe($observe);
